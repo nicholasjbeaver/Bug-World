dd412e7cdc85d99d51e3991a13996e7ee2796c90
Jim Beaver <git@jimbeaver.com>
2018-10-03 08:56:53 -0700

    Collision test working, destructors working, recursive check working

--- Collisions.py
+++ Collisions.py
@@ -1,38 +1,33 @@
 
 '''
 Collision API
-	Collisions() - container for the list of objects
-	CollisionObject() - necessary class to participate in the API
+	Collisions() - container for the list of objects.  There are two types: emitters and detectors.  Detectors only detect things in the emitters list.
+	CollisionObject() - necessary parent class to participate in the API
 
 	Usage:
-		1) Create a Collisions object
-		2) Add the types of objects that can collide and the method to call when they do
-		3) add the instances of the objects...destructor takes care of removing it from the collisions list if the object
-			is deleted
+		1) Create a Collisions object (CO)
+		2) The CO is responsible for know what collisions it needs to register for
+		3) The CO must have a pointer to its container.  This allows a recursive search to make sure it isn't "colliding" with itself
+		4) The CO must have a callback method to call when they do collide with something
+		5) The CO shouldn't asked to be removed from the lists when it is marked from deletion.
 
 	Definitions:
 		1) Hitbox is the object that gets detected.  Sometimes the hitbox is the object.  Other times, the hitbox is a child
 		   off of the object because it is offset from the object
 		2) Hitbox type will be used to use circles, bounding boxes etc.  Would like to do a cone for eyesight to get directionality
+		3) Maybe do a 1-d ray trace?
 
-
-
-
-	Used to hold references to objects that can collide
-	Detects collisions of those objecs
-	Calls a handler method depending on the type of the object
-	Defines an interface that collision objects must inherit/support for it to work
-	Support different bounding box, aka hit box types
-	recursively calls "isThisMe" to make sure it isn't colliding with itself or it's owner
-	does having emmitters and detectors and separate lists help with processing time?
-	register the hitboxes
-	the objects should know what lists they are on but should be encapsulated so don't have to know internal structure
 
 '''
 
 #Use transforms to represent absolute position, that way always know regardless of env
 
-#Have CollisionObject defined int the Collision class?
+
+'''
+	Class: CollisionObject
+		- Is a base class
+
+'''
 
 class CollisionObject():
 
@@ -42,7 +37,7 @@ class CollisionObject():
 
 	def __init__( Self ):
 		Self.set_hitbox_type( Self )
-		#must be added to in sublass so that if an object inherits this, it MUST know to add itself to a Collisions list.
+		#must be added too in sublass so that if an object inherits this, it MUST know to add itself to a Collisions list.
 
 	def get_abs_x(Self):
 		#must override
@@ -57,14 +52,15 @@ class CollisionObject():
 		pass
 
 	def get_hitbox_type( Self ):
-		#default will be circle
-		pass
+		return Self.hitbox_type
+
 
 	def set_hitbox_type( Self ):
 		Self.hitbox_type = Self.HitBoxType.HITBOX_CIRCLE
 
 	def is_this_me( Self, co ):
 		#check to see if the object to compare is itself or recursively its owner
+#TODO - add recursion for the container.  E.g., bug contains eyes.  Eyes contain a hitbox.
 		if( Self == co ): return True
 		else: return False
 
@@ -91,10 +87,19 @@ class Collisions():
 	def add_detector(Self, collision_object ):
 		Self._detectors.append( collision_object )
 
-	def remove_object(Self, collision_object ):
+	def del_emitter(Self, collision_object ):
+#TODO - since objects will be removed occasionally, can iterate through for each one.  Assume there isn't mass deletion because then it would be better to 
+#		mark for deletion and then process whole list
 		#find object in list and remove it
 		#should be called in the destructor method so that it is removed from all lists
-		pass
+		#See article: https://stackoverflow.com/questions/1207406/how-to-remove-items-from-a-list-while-iterating
+		#somelist[:] = [x for x in somelist if not determine(x)]
+		Self._emitters[:] = [co for co in Self._emitters if not co==collision_object]
+
+
+	def del_detector(Self, collision_object ):
+		Self._detectors[:] = [co for co in Self._detectors if not co==collision_object]
+		pass	
 
 	def print_collision( OB1, OB2 ):
 		print( OB1.name + ', ' + OB2.name  )
@@ -120,14 +125,7 @@ class Collisions():
 
 					#call the callback handler
 					Self._cb(CO1,CO2)
-'''
-	def detect_collisions( Self, hb1, hb2 )
-		if (hb1.hitbox_type != Collision.CollisionObject.hitbox_circle ):
-			pass
-		if (hb2.hitbox_type != Collision.CollisionObject.hitbox_circleCircleHitbox ):
-			pass
-		return Self.circle_collision( hb1, hb2 )
-'''
+
 
 
 #--- Testing Code after this point -------------------------------------------------------------------------------------------------------------
@@ -151,13 +149,14 @@ class CTOType():
 
 class CollisionTestObject( CollisionObject ):
 
-	def __init__( Self, CollisionTestWorld, name, x, y, size ):
+	def __init__( Self, CollisionTestWorld, Owner, name, x, y, size ):
 		Self.type = CTOType.OBJ #needs to be overwritten
 		Self.name = name
 		Self.x = x
 		Self.y = y
 		Self.size = size
 		Self.World = CollisionTestWorld #handle back to container so can call instance methods on it.
+		Self.Owner = Owner #for composite objects like eyes that are on the bug.  This would point to the bug
 
 	def __repr__( Self ):
 		return Self.name
@@ -169,38 +168,55 @@ class CollisionTestObject( CollisionObject ):
 		return Self.y
 
 	def get_size( Self ):
-		return Self.size	
+		return Self.size
+
+	def is_this_me( Self, CO2 ):
+		if 	( Self == CO2 ): True
+		elif ( Self.Owner != None ):
+			return ( Self.Owner.is_this_me( CO2 ))  #recursively call in case the comparision object is actually the owner.
+		else:
+			return False
+
+	def kill( Self ): #must override this method so that it is removed from all of the event lists
+		return
+
+	def __del__( Self ):
+		print("Destructor Called " + Self.name )
 
 
 class CollisionTestBody( CollisionTestObject ):
 
-	def __init__( Self, CTW, name, type, x, y, size ):
-		super().__init__( CTW, name, x, y, size )
+	def __init__( Self, CTW, Owner, name, type, x, y, size ):
+		super().__init__( CTW, Owner, name, x, y, size )
 		Self.type = type 
-		CTW.register_collision_event( CTW.CTWEventType.VISUAL_EMITTER, Self)
-		CTW.register_collision_event( CTW.CTWEventType.PHYSICAL_DETECTOR, Self )
-		CTW.register_collision_event( CTW.CTWEventType.PHYSICAL_EMITTER, Self )
+		Self.World.register_collision_event( Self.World.CTWEventType.VISUAL_EMITTER, Self)
+		Self.World.register_collision_event( Self.World.CTWEventType.PHYSICAL_DETECTOR, Self )
+		Self.World.register_collision_event( Self.World.CTWEventType.PHYSICAL_EMITTER, Self )
 
-	def __del__( Self ):
+	def kill( Self ):
+#TODO
 		#want to make sure it gets removed from all of the collision events
 		#since the world creates in different lists, it should remove it from all of them
 		#let's revisit though...probably should print a debug message here when something gets deleted at least
-		#Self.World.remove_object( Self )
-		pass
+		Self.World.deregister_collision_event( Self.World.CTWEventType.VISUAL_EMITTER, Self)
+		Self.World.deregister_collision_event( Self.World.CTWEventType.PHYSICAL_DETECTOR, Self )
+		Self.World.deregister_collision_event( Self.World.CTWEventType.PHYSICAL_EMITTER, Self )
+		print('About to die: ' + Self.name )
+
+
+
 
 
 class CollisionTestEye( CollisionTestObject ):
-	def __init__( Self, CTW, name, x, y, size ):
-		super().__init__( CTW, name, x, y, size )
+	def __init__( Self, CTW, Owner, name, x, y, size ):
+		super().__init__( CTW, Owner, name, x, y, size )
 		Self.type = CTOType.EYE 
-		CTW.register_collision_event( CTW.CTWEventType.VISUAL_DETECTOR, Self )
+		Self.World.register_collision_event( Self.World.CTWEventType.VISUAL_DETECTOR, Self )
 
-	def __del__( Self ):
-		#want to make sure it gets removed from all of the collision events
-		#since the world creates in different lists, it should remove it from all of them
-		#let's revisit though...probably should print a debug message here when something gets deleted at least
-		#Self.World.remove_object( Self )
-		pass
+	def kill( Self ):
+		Self.World.deregister_collision_event( Self.World.CTWEventType.VISUAL_DETECTOR, Self )
+		print('About to die: ' + Self.name )
+
 
 class CTWCollisionDict():
 
@@ -285,12 +301,30 @@ class CollisionTestWorld():
 		else:
 			print( "Unsupported Event Type: " + collision_event_type )
 
+	def deregister_collision_event( Self, collision_event_type, collision_object ):
+		#this abstracts the collision lists from the objects.  They just register for an event.  The world handles where it goes
+		if ( collision_event_type == Self.CTWEventType.PHYSICAL_EMITTER ):
+			print("Deleting Physical Emitter: " + str(collision_object) )
+			Self.PhysicalCollisions.del_emitter( collision_object )
+
+		elif( collision_event_type == Self.CTWEventType.PHYSICAL_DETECTOR):
+			print("Deleting Physical Detector: " + str(collision_object) )
+			Self.PhysicalCollisions.del_detector( collision_object)
+
+		elif( collision_event_type == Self.CTWEventType.VISUAL_EMITTER ):
+			print("Deleting Visual Emitter: " + str(collision_object) )
+			Self.VisualCollisions.del_emitter( collision_object )
+
+		elif( collision_event_type == Self.CTWEventType.VISUAL_DETECTOR ):
+			print("Deleting Visual Detector: " + str(collision_object) )
+			Self.VisualCollisions.del_detector( collision_object )
+		else:
+			print( "Unsupported Event Type: " + collision_event_type )
 
 	def check_for_collisions( Self ):
 		Self.PhysicalCollisions.detect_collisions()
 		Self.VisualCollisions.detect_collisions()
-		pass
-
+		
 
 	def add_bodies( Self ):
 		#x, y, size 
@@ -300,7 +334,7 @@ class CollisionTestWorld():
 		for pos in locs:
 			ctr += 1
 			name = "body" + str(ctr)
-			Self.Bodies.append( CollisionTestBody( Self, name, pos[0], pos[1], pos[2], pos[3] ) )
+			Self.Bodies.append( CollisionTestBody( Self, None, name, pos[0], pos[1], pos[2], pos[3] ) )
 
 	def add_eyes( Self ):
 		#x, y, size 
@@ -310,8 +344,15 @@ class CollisionTestWorld():
 		for pos in locs:
 			ctr += 1
 			name = "Eye" + str(ctr)
-			Self.Eyes.append( CollisionTestEye( Self, name, pos[0], pos[1], pos[2] ) )
+			Self.Eyes.append( CollisionTestEye( Self, None, name, pos[0], pos[1], pos[2] ) )
 
+	def del_body( Self ):
+#TODO - not sure this is correct way of handling...really 
+		#delete first object in list to see if destructors are called
+		to_kill = Self.Bodies[0]
+		to_kill.kill()
+		Self.Bodies[:] = Self.Bodies[2:]
+		del to_kill
 
 	def test_all( Self ):
 		#add the elements to the World
@@ -340,6 +381,11 @@ class CollisionTestWorld():
 		print()
 
 		Self.check_for_collisions()
+		print("After Collision Check")
+		print()
+
+		Self.del_body()
+		print("After delete body")
 
 		#Test cases
 
