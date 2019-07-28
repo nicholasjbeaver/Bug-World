04da2e9f1912a87c0303b2077af46c7ca0b133a2
Jim Beaver <home@jimbeaver.com>
2019-07-28 15:22:38 -0700

    Added space key to pause the simulation

--- main.py
+++ main.py
@@ -13,23 +13,28 @@ from BugWorld import *
 #main control loop of the pygame
 class BugSim( PygameHelper ):
 	
-	def __init__(Self):
-		Self.BW = BugWorld() #instantiate the world and its objects
-		super(BugSim,Self).__init__( (Self.BW.BOUNDARY_WIDTH, Self.BW.BOUNDARY_HEIGHT), Color.WHITE )
+	def __init__(self):
+		self.BW = BugWorld()  # instantiate the world and its objects
+		super(BugSim, self).__init__((self.BW.BOUNDARY_WIDTH, self.BW.BOUNDARY_HEIGHT), Color.WHITE)
+		self.pause = False
 
-	def update(Self): #update everything in the world
-		Self.BW.update()
+	def update(self):  # update everything in the world
+		if not self.pause:
+			self.BW.update()
 
-
-	def draw( Self ): #draw the resulting world
-		Self.screen.fill(Color.WHITE)
-		Self.BW.draw(Self.screen)
+	def draw(self):  # draw the resulting world
+		self.screen.fill(Color.WHITE)
+		self.BW.draw(self.screen)
 		pygame.display.update()
 
-	def keyDown(Self, key):
+	def keyDown(self, key):
 		
-		if key == K_SPACE:
-			pass
+		if key == K_SPACE:  # pause the game
+			if self.pause:
+				self.pause = False
+			else:
+				self.pause = True
+
 		elif key == K_LEFT:
 			pass
 		elif key == K_RIGHT:
@@ -37,8 +42,17 @@ class BugSim( PygameHelper ):
 		else:
 			print(key)
 
-		
+	def keyUp(self, key):
+		pass
+
+	def mouseUp(self, pos):
+		pass
+
+	def mouseUp2(self, pos):
+		pass
+
+
 if __name__ == "__main__":
 	g = BugSim()
 	g.mainLoop(60)
-    
+
